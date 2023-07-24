1dd2a53a73406a88ad2cb506e8e5f714fe8995e9
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-07-24 11:39:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+canvas = document.getElementById('nyCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;  
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown)
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="Você pressiounou a tecla do alfabeto ";
+			console.log("aplhabet key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="Você pressiounou a tecla de outras";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="alfabeto.png";
+	add();	
+}
+function numberkey()
+{
+	img_image="número.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="direcional.png";
+	add();
+}
+function specialkey()
+{
+	img_image="especial.png";
+	add();	
+}
+function otherkey()
+{
+	img_image="outras.png";
+	add();
+}
+	
